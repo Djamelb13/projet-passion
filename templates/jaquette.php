4d825a8dfe2bfce454ec1ad02f1ea26a26f90516
Djamelb13 <DWWM-DJAMEL-BENNABOU@mode83.onmicrosoft.com>
2023-09-11 09:04:46 +0200

    mise a jour de la jaquette save importante

--- templates/jaquette.php
+++ templates/jaquette.php
@@ -12,21 +12,30 @@
     //     LEFT JOIN tags ON gametags.tag_id = tags.tag_id
     //     LEFT JOIN propose_une_vente ON jeu.game_title = propose_une_vente.game_title
     //     GROUP BY jeu.game_title, jeu.game_img, jeu.game_date, jeu.comm_id, propose_une_vente.sell_price";
-    $user_name = $_SESSION['user_name'];
-    $sql = "SELECT jeu.game_title, GROUP_CONCAT(jeu.game_desc) AS game_desc, jeu.game_img, jeu.game_date, jeu.comm_id, GROUP_CONCAT(tags.tag_name SEPARATOR ', ') AS game_tags, usercollection.coll_name
-    FROM usercollection
-    INNER JOIN compose ON usercollection.coll_id = compose.coll_id
-    INNER JOIN jeu ON compose.game_id = jeu.game_id
-    LEFT JOIN gametags ON jeu.game_id = gametags.game_id
-    LEFT JOIN tags ON gametags.tag_id = tags.tag_id
-    WHERE usercollection.user_name = :user_name
-    GROUP BY jeu.game_title, jeu.game_img, jeu.game_date, jeu.comm_id, usercollection.coll_name";
+//     $user_name = $_SESSION['user_name'];
+//     $sql = "SELECT jeu.game_title, GROUP_CONCAT(jeu.game_desc) AS game_desc, jeu.game_img, jeu.game_date, jeu.comm_id, GROUP_CONCAT(tags.tag_name SEPARATOR ', ') AS game_tags, usercollection.coll_name
+//     FROM usercollection
+//     INNER JOIN compose ON usercollection.coll_id = compose.coll_id
+//     INNER JOIN jeu ON compose.game_id = jeu.game_id
+//     LEFT JOIN gametags ON jeu.game_id = gametags.game_id
+//     LEFT JOIN tags ON gametags.tag_id = tags.tag_id
+//     WHERE usercollection.user_name = :user_name
+//     GROUP BY jeu.game_title, jeu.game_img, jeu.game_date, jeu.comm_id, usercollection.coll_name";
 
-$result = $connexion->prepare($sql);
-$result->bindParam(':user_name', $user_name, PDO::PARAM_STR);
-$result->execute();
+// $result = $connexion->prepare($sql);
+// $result->bindParam(':user_name', $user_name, PDO::PARAM_STR);
+// $result->execute();
 
-    $result = $connexion->query($sql);
+    // $result = $connexion->query($sql);
+$sql = "SELECT jeu.game_title, jeu.game_desc, jeu.game_img, jeu.game_date, jeu.comm_id, GROUP_CONCAT(tags.tag_name SEPARATOR ', ') AS game_tags, propose_une_vente.sell_price
+        FROM jeu
+        LEFT JOIN gametags ON jeu.game_id = gametags.game_id
+        LEFT JOIN tags ON gametags.tag_id = tags.tag_id
+        LEFT JOIN propose_une_vente ON jeu.game_title = propose_une_vente.game_title
+        GROUP BY jeu.game_title, jeu.game_desc, jeu.game_img, jeu.game_date, jeu.comm_id, propose_une_vente.sell_price";
+$result = $connexion->query($sql);
+
+
 ?>
 <div class="container">
 <?php
