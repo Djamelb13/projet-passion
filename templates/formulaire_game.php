c27369f8a3c50105a1c40573d8c62b94dedde07f
Djamelb13 <DWWM-DJAMEL-BENNABOU@mode83.onmicrosoft.com>
2023-09-18 16:45:19 +0200

    resolution conflit tag

--- templates/formulaire_game.php
+++ templates/formulaire_game.php
@@ -22,6 +22,7 @@
                                     Sélectionnez des tags
                                 </button>
                                 <ul class="dropdown-menu" aria-labelledby="tagsDropdown">
+                                
                                     <?php
                                     try {
                                         // Connexion à la base de données (à personnaliser avec vos paramètres)
@@ -101,7 +102,7 @@
     </div>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-<script src="../js/form.js">
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> -->
+<!-- <script src="../js/form.js"> -->
 
 </script>
